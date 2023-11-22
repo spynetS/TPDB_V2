bdd9daec008fbc5d8955fe1466a92dd07921542a
Ali Al Rashini <44705253+SharkooMaster@users.noreply.github.com>
2023-11-22 04:24:53 +0100

    Unit testing StrnCat()

--- src/Tests/UnitTests.c
+++ src/Tests/UnitTests.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "../Errors/I_Errors.h"
+#include "../Utils/StringTools.h"
 #include "../Interface/I_Database.h"
 #include "../Interface/I_Table.h"
+
+enum TP_ERROR_TYPES TP_TEST_STRNCAT()
+{
+	printf("--|TP_TEST_STRNCAT|--: ...");
+	char *test_1 = TP_StrnCat("", 2, "hello ", "world!");
+	char *test_2 = TP_StrnCat("How ", 2, "are ", "you?");
+
+	if(strcmp(test_1, "hello world!") == 0 && strcmp(test_2, "How are you?") == 0)
+	{
+		free(test_1);
+		free(test_2);
+		printf(ERROR_ASCII_SUCCESS);
+		printf("\n");
+		return TP_SUCCESS;
+	}
+	else
+	{
+		free(test_1);
+		free(test_2);
+		printf(ERROR_ASCII_FAIL);
+		printf("\n");
+		return TP_FAILED_STRNCAT;
+	}
+}
 
 enum TP_ERROR_TYPES TP_TEST_CreateTPDatabase()
 {
@@ -53,7 +79,10 @@ enum TP_ERROR_TYPES TP_TEST_CreateTPTable()
 int main()
 {
 	puts("--|UnitTest|--");
+	TP_CheckError(TP_TEST_STRNCAT());
+
 	TP_CheckError(TP_TEST_CreateTPDatabase());
 	TP_CheckError(TP_TEST_CreateTPTable());
+	
 	exit(0);
 }
